7178dd5d571862eaca04a81ff18a8de359f2cc02
aran <aran@apexsoft.co.kr>
2018-09-10 17:28:24 +0900

    chat users edit

--- index.js
+++ index.js
@@ -11,25 +11,7 @@ var playerList = [
 ];
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-
-// 리스트 만들기
-function List() {
-    this.elements = {};
-    this.idx = 0;
-    this.length = 0;
-}
-List.prototype.add = function(element) {
-    this.length++;
-    this.elements[this.idx++] = element;
-};
-
-List.prototype.get = (idx)=> {
-    return this.elements[idx];
-};
-
-// 접속한 유저 리스트
-var userList = new List();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // 서버 포트
 http.listen(2700, '0.0.0.0');
@@ -39,49 +21,47 @@ require('dns').lookup(require('os').hostname(), function (err, add, fam) {
 });
 
 // 메인 페이지 접속
-app.post('/room', (req, res) =>{
+app.post('/room', (req, res) => {
     nick = req.body.nameInput;
     res.render(path.join('chat.ejs'));
 });
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.render('name.ejs');
 });
 
 // 유저 접속
-io.on('connection', (socket)=> {
-    // console.log(nick + '-------in');
-
+io.on('connection', (socket) => {
     // 중복유저를 확인하고 유저목록 저장
-    if(!(nick=='')){
-        var player = { joinnick : '', joinaddr : '' }
+    var player = { joinnick: '', joinaddr: '' }
+    if (!(nick == '')) {
+        // var player = { joinnick: '', joinaddr: '' }
         var chNick = nick;
         var chAddr = socket.client.conn.remoteAddress;
-        
+
         // 이미 있는 닉네임인지 확인
-        var checkNick = playerList.filter((player)=>{
-            return player.joinnick==chNick
+        var checkNick = playerList.filter((player) => {
+            return player.joinnick == chNick
         });
-        
+
         // 이미 접속한 ip인지 확인
-        var checkAddr = playerList.filter((player)=>{
-            return player.joinaddr==chAddr
+        var checkAddr = playerList.filter((player) => {
+            return player.joinaddr == chAddr
         });
-        
+
         // 체크 후 문제가 없으면 유저를 저장한다.
-        if(checkNick=='' && checkAddr==''){
+        if (checkNick == '' && checkAddr == '') {
             player.joinnick = chNick;
             player.joinaddr = chAddr;
             playerList.push(player);
         };
     };
 
-    console.log(playerList);
-
-    io.emit('guestIn', nick);
-    // userList.add(userList.length);
+    // io.emit('guestIn', nick);
 
     socket.emit('user join', nick);
+
+    io.emit('user list', playerList);
 
     // 채팅 업데이트
     socket.on('chat message', function (msg, userName) {
@@ -90,11 +70,11 @@ io.on('connection', (socket)=> {
 
     // 퇴장하면 접속 유저 목록에서 제외해준다.
     socket.on('disconnect', function () {
-        var s = playerList.findIndex((player)=>{
-            return player.joinnick==chNick
-        })
-        playerList.splice(s, 1);
-        console.log('user out-----------------------');
-        io.emit('guestOut', nick);
+        var s = playerList.findIndex((player) => {
+            return player.joinnick == chNick
         });
+        playerList.splice(s, 1);
+        io.emit('user list', playerList);
+        // io.emit('guestOut', nick);
+    });
 });
